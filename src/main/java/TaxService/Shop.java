1091cdbed21924e9efeccbd549d71bdc37c3ca86
Kseniia <xenia-nikole@mail.ru>
2021-07-30 11:03:58 +0300

    correct second task

--- src/main/java/TaxService/Shop.java
+++ src/main/java/TaxService/Shop.java
@@ -4,7 +4,8 @@ import java.util.concurrent.atomic.LongAdder;
 
 public class Shop {
     private String name;
-    private long revenue = (long) (Math.random()*1_000_000);
+    private long revenue;
+    private int time = (int) (Math.random()*(3600*24));
     private LongAdder longAdder;
 
     public Shop(String name, LongAdder longAdder) {
@@ -13,7 +14,11 @@ public class Shop {
     }
 
     public void calculate() {
-        System.out.printf("\nСегодняшняя выручка магазина %s составляет: " + revenue, name);
-        longAdder.add(revenue);
+        for (int i = 0; i < 500; i++) {
+            revenue = (long) (Math.random()*1_000_000);
+            System.out.printf("\nБыла совершена покупка в магазине %s на %d рублей", name, revenue);
+            longAdder.add(revenue);
+        }
+        System.out.printf("\nСегодняшняя выручка магазина %s составляет : " + longAdder, name);
     }
 }
